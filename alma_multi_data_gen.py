fc753d513dc2dee8b6d01965ab56ddac520a6390
qwopqwop200 <qwopqwop200@gmail.com>
2024-03-09 19:45:27 +0900

    fix bug

--- alma_multi_data_gen.py
+++ alma_multi_data_gen.py
@@ -32,7 +32,7 @@ data_mt = joblib.load('./data_mt.pkl')
 prompts_all = []
 for i in range(len(data_mt['ko'])):
     if os.path.exists(f'{result_path}{i}.pkl'):
-        print(f"Skipping {index}")
+        print(f"Skipping {i}")
     else:
         prompts_all.append([i, data_mt['en'][i],data_mt['ko'][i]])
 
