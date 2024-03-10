0384eb27104dc5de1374edfcddb76f9d78b59904
qwopqwop200 <qwopqwop200@gmail.com>
2024-03-10 15:22:25 +0900

    Add files via upload

---
+++ comet_save.py
@@ -0,0 +1,53 @@
+import joblib
+from comet import download_model, load_from_checkpoint
+
+output  = joblib.load('./output.pkl')
+
+datas = []
+
+for data in output:
+    datas.append(
+        {
+            "src": data['en'], 
+            "mt": data['ko'], 
+        }
+    )
+    datas.append(
+        {
+            "src": data['ko'], 
+            "mt": data['en'], 
+        }
+    )
+
+    
+    datas.append(
+        {
+            "src": data['en'], 
+            "mt": data['alma_ko'], 
+        }
+    )
+    datas.append(
+        {
+            "src": data['ko'], 
+            "mt": data['alma_en'], 
+        }
+    )
+
+    
+    datas.append(
+        {
+            "src": data['en'], 
+            "mt": data['gpt4_ko'], 
+        }
+    )
+    datas.append(
+        {
+            "src": data['ko'], 
+            "mt": data['gpt4_en'], 
+        }
+    )
+
+model_path = download_model("Unbabel/XCOMET-XXL")
+model = load_from_checkpoint(model_path)
+model_output = model.predict(datas, batch_size=1, gpus=1)
+joblib.dump(model_output, './xcomet.pkl')
